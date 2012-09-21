c47561b4f7b458cac7536bb4062242886e5722ba
Jerry Siu <jerry@onyx-international.com>
2012-09-21 18:37:41 +0800

    Add show standby logo tool

---
+++ fsl/imx50_rdp/logo/Android.mk
@@ -0,0 +1,35 @@
+LOCAL_PATH:= $(call my-dir)
+include $(CLEAR_VARS)
+
+LOCAL_SRC_FILES:= \
+	logo_main.cpp \
+	Logo.cpp
+
+# need "-lrt" on Linux simulator to pick up clock_gettime
+ifeq ($(TARGET_SIMULATOR),true)
+	ifeq ($(HOST_OS),linux)
+		LOCAL_LDLIBS += -lrt
+	endif
+endif
+
+LOCAL_CFLAGS += -DGL_GLEXT_PROTOTYPES -DEGL_EGLEXT_PROTOTYPES
+
+LOCAL_SHARED_LIBRARIES := \
+	libcutils \
+	libutils \
+	libbinder \
+    libui \
+	libskia \
+    libEGL \
+    libGLESv1_CM \
+	libhardware_legacy \
+    libsurfaceflinger_client
+
+LOCAL_C_INCLUDES := \
+	$(call include-path-for, corecg graphics)
+
+LOCAL_MODULE_TAGS := eng
+LOCAL_MODULE:= logo
+
+
+include $(BUILD_EXECUTABLE)
